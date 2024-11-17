b2e140f207e68ab41c2661db18e74685cb1d38b1
siyavash <siavash.khani200@gmail.com>
2024-11-17 08:52:32 +0330

    feat(Login, Signup): fixed responsive

--- components/Site/Auth/Signup.tsx
+++ components/Site/Auth/Signup.tsx
@@ -55,7 +55,7 @@ const Signup = () => {
               Create an Account
             </h2>
 
-            <div className="flex items-center gap-8">
+            <div className="flex flex-col sm:flex-row items-center sm:gap-8">
               <button
                 aria-label="signup with google"
                 className="text-body-color dark:text-body-color-dark dark:shadow-two mb-6 flex w-full items-center justify-center rounded-sm border border-stroke bg-[#f8f8f8] px-6 py-3 text-base outline-none transition-all duration-300 hover:border-primary hover:bg-primary/5 hover:text-primary dark:border-transparent dark:bg-[#2C303B] dark:hover:border-primary dark:hover:bg-primary/5 dark:hover:text-primary dark:hover:shadow-none"
