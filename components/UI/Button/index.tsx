de012a565129dfe50f674a933994ddc8666f95b3
siyavash <siavash.khani200@gmail.com>
2024-11-17 09:44:57 +0330

    update: added button input

---
+++ components/UI/Button/index.tsx
@@ -0,0 +1,31 @@
+import { forwardRef } from "react";
+import { twMerge } from "tailwind-merge";
+
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: string;
+}
+
+const Button = forwardRef<HTMLButtonElement, Props>(
+  ({ className, children, disabled, type = "button", ...props }, ref) => {
+    return (
+      <button
+        type={type}
+        className={twMerge(
+          `
+    w-full rounded-full border border-transparent bg-indigo-500 px-3 py-3 font-bold text-black transition hover:opacity-75 disabled:cursor-not-allowed disabled:opacity-50
+    `,
+          className,
+        )}
+        disabled={disabled}
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  },
+);
+
+Button.displayName = "Button";
+
+export default Button;
