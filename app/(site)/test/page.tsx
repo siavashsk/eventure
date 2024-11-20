7198c66c7c3e4318511e2becbff2bfda33d08c21
siyavash <siavash.khani200@gmail.com>
2024-11-20 14:50:00 +0330

    feat(UI): update footer

--- app/(site)/test/page.tsx
+++ app/(site)/test/page.tsx
@@ -6,7 +6,7 @@ const Test = () => {
   return (
     <div className="h-[100vh] w-full">
       <div className="mt-27 h-full w-full p-8">
-        <Chip profile>helloo w World</Chip>
+        
       </div>
     </div>
   );
