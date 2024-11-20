b72b37ad9fd1688890cd99faab0b34fb06feed9e
siyavash <siavash.khani200@gmail.com>
2024-11-20 09:34:53 +0330

    feat(UI): implemented Button and checkbox with Best practice

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/UI";
+import { Checkbox } from "@/components/UI/Checkbox";
 import React from "react";
 
 const DashboardPage = () => {
   return (
     <div className="mt-8">
+      <Checkbox label="hello world"/>
     </div>
   );
 };
--- components/UI/Checkbox/index.tsx
+++ components/UI/Checkbox/index.tsx
@@ -1,17 +1,37 @@
-export function Checkbox() {
+import React, { InputHTMLAttributes } from "react";
+
+interface CheckboxProps
+  extends Omit<InputHTMLAttributes<HTMLInputElement>, "type"> {
+  label?: string;
+  id?: string;
+  checked?: boolean;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export const Checkbox: React.FC<CheckboxProps> = ({
+  label,
+  id = "checkbox",
+  checked,
+  onChange,
+  className,
+  ...rest
+}) => {
   return (
     <div className="inline-flex items-center">
       <label
         className="relative flex cursor-pointer items-center rounded-full"
-        htmlFor="checkbox"
+        htmlFor={id}
       >
         <input
-          //   onChange={checkBoxPrivateShenase}
           type="checkbox"
-          defaultChecked={false}
-          //   checked={privateCheckBoxShenase}
-          className="before:content[''] border-primary-onContainer before:bg-primary-onContainer2 checked:border-primary-main checked:bg-primary-main checked:before:bg-primary-main peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border transition-all before:absolute before:left-2/4 before:top-2/4 before:block before:h-12 before:w-12 before:-translate-x-2/4 before:-translate-y-2/4 before:rounded-full before:opacity-0 before:transition-opacity hover:before:opacity-10"
-          id="checkbox"
+          id={id}
+          checked={checked}
+          onChange={onChange}
+          className={`checked:border-indigo-500 checked:bg-indigo-500 checked:before:bg-indigo-500 border-primary-onContainer before:bg-primary-onContainer2 peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border transition-all before:absolute before:left-2/4 before:top-2/4 before:block before:h-12 before:w-12 before:-translate-x-2/4 
+          before:-translate-y-2/4 before:rounded-full before:opacity-0 
+          before:transition-opacity hover:before:opacity-10
+          ${className || ""}`}
+          {...rest}
         />
         <span className="pointer-events-none absolute left-2/4 top-2/4 -translate-x-2/4 -translate-y-2/4 text-white opacity-0 transition-opacity peer-checked:opacity-100">
           <svg
@@ -30,6 +50,7 @@ export function Checkbox() {
           </svg>
         </span>
       </label>
+      {label && <span className="ml-2">{label}</span>}
     </div>
   );
-}
+};
